e0f147fc8d63a298316ab082d2c6eb3e64e159fa
Amina-Moufakkir <aminamouf22@gmail.com>
2023-03-11 23:59:39 -0500

    Restructured project folders and added custom CSS

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import './App.css';
-
-// import NavBar from './Components/NavBar';
-import Home from './Components/Home';
-import About from './Components/About';
-import SearchBar from './Components/SearchBar';
+import { Routes, Route } from 'react-router-dom';
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import About from "./pages/About";
+import SingleVid from "./pages/SingleVid";
+import Error from "./pages/Error";
 
 function App() {
-  console.log(process.env.React_APP_API_Key)
+  // console.log(process.env.React_APP_API_Key)
   return (
-    <div className="App">
-      {/* <NavBar/> */}
-  
-
-    <h1>Youtube Clone</h1>
-
-    <h2>videos</h2>
-
-      {/* </Routes> */}
-      <h3>WORD</h3>
-    </div>
+    <>
+     <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/video/:id" element={<SingleVid />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+     
+       
+    </>
   );
 }
 
