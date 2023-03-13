7eb775825946c7a271347bae416269a5238999ee
JohnnyMCR <jmcastillo991@gmail.com>
2023-03-13 19:17:13 -0400

    About.js updated

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,10 +3,9 @@ const About = () => {
     <section className="section about-section">
       <h1 className="section-title">about us</h1>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
-        repudiandae architecto qui adipisci in officiis, aperiam sequi atque
-        perferendis eos, autem maiores nisi saepe quisquam hic odio consectetur
-        nobis veritatis quasi explicabo obcaecati doloremque
+        Johnny Castillo GitHub: https://github.com/JohnnyMCR
+        Johnny Castillo LinkedIn: https://www.linkedin.com/in/johnnycastillo
+
       </p>
     </section>
   )
