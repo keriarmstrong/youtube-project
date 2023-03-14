1aa09202a7343c4604c922c4db59c70f0ab389d1
JohnnyMCR <jmcastillo991@gmail.com>
2023-03-13 21:32:51 -0400

    Updated About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,12 +1,32 @@
 const About = () => {
   return (
     <section className="section about-section">
-      <h1 className="section-title">about us</h1>
-      <p>
-        Johnny Castillo GitHub: https://github.com/JohnnyMCR
-        Johnny Castillo LinkedIn: https://www.linkedin.com/in/johnnycastillo
+      <h1 className="section-title"><center>About Us</center></h1>
+      This is our YouTube Clone project. Search all your favorite YouTube videos on the SearchBar. If you're ever lost, click the Home or YouTube Clone button at the top to return. The following people worked hard on this project:
+      <br />
+      <br />
+      <div><b><center>Amina Moufakkir</center></b></div>
+      <div><u><a href="https://github.com/Amina-Moufakkir"><center><b>GitHub</b></center></a></u></div>
+      <div><u><a href="https://www.linkedin.com/in/amina-moufakkir-2a7500244/"><center><b>LinkedIn</b></center></a></u></div>
+      <br />
+      As a Full Stack Developer, I bring to the table a diverse set of interests that shape my perspective and approach to my work. In my free time, I enjoy exploring the culinary arts and delving into books that broaden my understanding of the world. These hobbies have taught me the value of patience, attention to detail, and following a well-defined process - skills that directly transfer to my work as a developer.
+      <br />
+      <div><b><center>Keri Emmanuel</center></b></div>
+      <div><u><a href="https://github.com/keriarmstrong"><center><b>GitHub</b></center></a></u></div>
+      <div><u><a href="https://www.linkedin.com/in/keri-armstrong/"><center><b>LinkedIn</b></center></a></u></div>
+      <br />
 
-      </p>
+      <br />
+      <div><b><center>Johnny Castillo</center></b></div>
+      <div> <u><a href="https://github.com/JohnnyMCR"><center><b>GitHub</b></center></a></u></div>
+      <div> <u><a href="https://www.linkedin.com/in/johnnycastillo"><center><b>LinkedIn</b></center></a></u></div>
+      <br />
+      Budding Software Engineer from a Recruitment/Coordination background. I'm passionate about learning new and adventurous ways of seeking knowledge and using it to create interesting programs. I'm a well rounded individual who believes anything can be referenced with food including coding.
+      <br />
+      <br />
+      <i>"A recipe has no soul. You, as the cook must bring the soul to the recipe."
+        - Thomas Keller</i>
+
     </section>
   )
 }
