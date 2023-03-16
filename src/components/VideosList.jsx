2fdbab27195b4845349d4327ed3f2886cb31bee2
JohnnyMCR <jmcastillo991@gmail.com>
2023-03-15 20:11:32 -0400

    Fixing bugs

--- src/components/VideosList.jsx
+++ src/components/VideosList.jsx
@@ -4,9 +4,6 @@ const VideosList = ({ category, videoList }) => {
 
   return (
     <section className="section">
-      <section className="category">
-        <div><h3>{category} <span style={{color: "red"}}>Videos</span></h3></div>
-      </section>
       <div className="thumbnails-center">
         {videoList.map((video) => {
           return <Video video={video} key={video.etag} />
